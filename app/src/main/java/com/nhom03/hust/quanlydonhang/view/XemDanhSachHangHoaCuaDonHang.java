14a0f1e2894a8b45779331c0d5f80f40eaaa5add
sakura <trieuvandung95@gmail.com>
2016-12-06 14:01:37 +0700

    ok

---
+++ app/src/main/java/com/nhom03/hust/quanlydonhang/view/XemDanhSachHangHoaCuaDonHang.java
@@ -0,0 +1,19 @@
+package com.nhom03.hust.quanlydonhang.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nhom03.hust.quanlydonhang.R;
+
+/**
+ * Created by sakura on 06/12/2016.
+ */
+
+public class XemDanhSachHangHoaCuaDonHang extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xem_chi_tiet_khach_hang);
+
+    }
+}
