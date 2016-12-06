b16926bd3e6201323279d9861ea63ecb482c352d
sakura <trieuvandung95@gmail.com>
2016-12-06 14:27:49 +0700

    ok

--- app/src/main/java/com/nhom03/hust/quanlydonhang/view/XemDanhSachHangHoaCuaDonHang.java
+++ app/src/main/java/com/nhom03/hust/quanlydonhang/view/XemDanhSachHangHoaCuaDonHang.java
@@ -2,6 +2,7 @@ package com.nhom03.hust.quanlydonhang.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import com.nhom03.hust.quanlydonhang.R;
 
@@ -11,9 +12,13 @@ import com.nhom03.hust.quanlydonhang.R;
 
 public class XemDanhSachHangHoaCuaDonHang extends AppCompatActivity {
 
+    private ListView listHangHoa;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.xem_chi_tiet_khach_hang);
+        setContentView(R.layout.xem_danh_sach_hang_hoa_trong_don_hang);
+
+        listHangHoa = (ListView) findViewById(R.id.list_hang_hoa_cua_don_hang);
+
 
     }
 }
